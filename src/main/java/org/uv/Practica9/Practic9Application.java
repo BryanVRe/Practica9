37e5fb67d4e3ca8993099440a60d50333a19d685
carte <fsocietybvr@gmail.com>
2023-05-05 10:53:19 -0500

    initialization

---
+++ src/main/java/org/uv/Practica9/Practic9Application.java
@@ -0,0 +1,13 @@
+package org.uv.Practica9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practic9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practic9Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/uv/Practica9/Practic9ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uv.Practica9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practic9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
