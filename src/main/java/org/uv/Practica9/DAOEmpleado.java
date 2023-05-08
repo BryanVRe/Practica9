fa2c63a24da5af83d3cc5ff1c4de918ae00c6545
carte <fsocietybvr@gmail.com>
2023-05-08 10:57:36 -0500

    CRUD

---
+++ src/main/java/org/uv/Practica9/DAOEmpleado.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.uv.Practica9;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author carte
+ */
+public interface DAOEmpleado extends JpaRepository<Empleado, Long>{
+    
+}
